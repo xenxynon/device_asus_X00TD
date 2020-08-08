a311283d1d9ddf2b01392df777cf04c636df97bb
Nolen Johnson <johnsonnolen@gmail.com>
2020-08-08 09:51:07 +0530

    X00TD: Import extract files scripts

--- extract-files.sh
+++ extract-files.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 #
+<<<<<<< HEAD
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2020 The LineageOS Project
+=======
+# Copyright (C) 2020 The LineageOS Project
+>>>>>>> b342f4c (X00TD: Import Lineage's commmon extract_utils)
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+<<<<<<< HEAD
 set -e
 
 # Load extract_utils and do some sanity checks
@@ -88,3 +93,21 @@ if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
+=======
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+# Required
+export DEVICE=X00TD
+export DEVICE_COMMON=sdm660-common
+export VENDOR=asus
+
+export DEVICE_BRINGUP_YEAR=2020
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+>>>>>>> b342f4c (X00TD: Import Lineage's commmon extract_utils)
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,6 +8,9 @@
 
 set -e
 
+DEVICE=X00TD
+VENDOR=asus
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
@@ -21,28 +24,38 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
-# Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
 
-# Warning headers and guards
-write_headers "X00TD X01BD"
+KANG=
+SECTION=
 
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
+    esac
+    shift
+done
 
-# Finish
-write_footers
-
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
-
-    # Warning headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
-
-    # Finish
-    write_footers
+if [ -z "${SRC}" ]; then
+    SRC="adb"
 fi
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+"${MY_DIR}/setup-makefiles.sh"
